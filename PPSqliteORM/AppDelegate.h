6053002d1bb9f7a9292168a9ce0ee14b7cafcc27
smallmuou <lvyexuwenfa100@126.com>
2014-11-25 16:31:16 +0800

    Initial Commit

---
+++ PPSqliteORM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPSqliteORM
+//
+//  Created by StarNet on 14/11/25.
+//  Copyright (c) 2014年 StarNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PPSqliteORM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PPSqliteORM
+//
+//  Created by StarNet on 14/11/25.
+//  Copyright (c) 2014年 StarNet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
